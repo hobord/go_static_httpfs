47ea17e55c158a0b1effd9cc39331c8b0d32ab69
Balázs Szabó <balazs.szabo@gmail.com>
2019-05-12 13:19:01 +0300

    tab to spaces

--- server.go
+++ server.go
@@ -1,15 +1,15 @@
 /*
 Serve is a very simple static file server in go
 Usage:
-	-p="8100" or env.PORT:                 port to serve on
-	-d="." or env.DIRECTORY:               the directory of static files to host
-	-b="/" or env.BASE_URI:                base path of static files on the web
-	-k="300" or env.KEEPALIVE:             http header keep-alive value
-	-c="max-age=2800" or env.CACHECONTROL: http header Cache-controll: value
-	-i="true" or env.DIRINDEX:             show directories index
-	-l="true" or env.LOG:                  show requests logs
-	-m="true" or env.METRICS:              generate / serve metrics
-	-mp="9090" or env.METRICS_PORT:        serve metrics on port
+  -p="8100" or env.PORT:                 port to serve on
+  -d="." or env.DIRECTORY:               the directory of static files to host
+  -b="/" or env.BASE_URI:                base path of static files on the web
+  -k="300" or env.KEEPALIVE:             http header keep-alive value
+  -c="max-age=2800" or env.CACHECONTROL: http header Cache-controll: value
+  -i="true" or env.DIRINDEX:             show directories index
+  -l="true" or env.LOG:                  show requests logs
+  -m="true" or env.METRICS:              generate / serve metrics
+  -mp="9090" or env.METRICS_PORT:        serve metrics on port
 */
 package main
 
